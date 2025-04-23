c6e11c822a2c36a325f3ae502c4b0d940e9ec576
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-23 10:39:53 +0200

    refactor: simplify getPurchase amount
    - add the output request to input the amount
    - remove extra line of code to make the func more efficient

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
 fun getPurchaseAmount(): Int {
+    println("Please enter the purchase amount.")
     val purchaseAmount = Console.readLine()
-    val intPurchaseAmount = purchaseAmount.toInt()
-    return intPurchaseAmount
+    return purchaseAmount.toInt()
 }
 
 fun main() {
-
+    getPurchaseAmount()
 }
