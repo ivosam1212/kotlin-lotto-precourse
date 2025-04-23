658e56f0f55f8e924f91d264774f81512af2471f
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-23 10:46:59 +0200

    feat: add validatePurchaseAmount func
    - validate the purchase amount to check if it's higher than 1000
    - use an IllegalArgumentException in case it is not 1000 or more
    - use the [ERROR] syntax

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -3,12 +3,17 @@ package lotto
 import camp.nextstep.edu.missionutils.Randoms
 import camp.nextstep.edu.missionutils.Console
 
-fun getPurchaseAmount(): Int {
+private fun getPurchaseAmount(): Int {
     println("Please enter the purchase amount.")
     val purchaseAmount = Console.readLine()
     return purchaseAmount.toInt()
 }
 
+private fun validatePurchaseAmount(purchaseAmount: Int) {
+    if (purchaseAmount < 1000)
+        throw IllegalArgumentException("[ERROR] The purchase amount must be 1000 or higher")
+}
+
 fun main() {
-    getPurchaseAmount()
+    validatePurchaseAmount(getPurchaseAmount())
 }
