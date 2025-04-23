d22bae070e69d5ceb4b91290abfe82131c687620
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-23 10:36:54 +0200

    feat: add getPurchaseAmount func
    - add a function to get the purchase amount using Console.readLine()
    - return the purchase a mount ot be used in main

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,14 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+fun getPurchaseAmount(): Int {
+    val purchaseAmount = Console.readLine()
+    val intPurchaseAmount = purchaseAmount.toInt()
+    return intPurchaseAmount
+}
+
 fun main() {
-    // TODO: Implement the program
+
 }
