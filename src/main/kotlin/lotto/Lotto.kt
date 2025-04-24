7d0c21c5f5b95c743665734c07b29928c9633e81
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-24 21:45:46 +0200

    feat: add validateNumbers function
    - check if winning numbers are between 1 and 45
    - shows an error if the number is invalid

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,14 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        validateNumbers(numbers)
     }
 
     // TODO: Implement additional functions
+    fun validateNumbers(numbers: List<Int>) {
+        for (number in numbers) {
+            if ((number < 1 || number > 45))
+                throw IllegalArgumentException("[ERROR] Lotto numbers must be between 1 and 45.")
+        }
+    }
 }
