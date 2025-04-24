7e2003d1fa109035485b59c2e88ee9b6b8f70408
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-24 22:01:01 +0200

    feat: add a validateUniqueNumbers function
    - check if numbers on the winning list are unique
    - create a validattions function to call validateNumbers and validateUniqueNumbers

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -1,16 +1,30 @@
 package lotto
 
+import org.assertj.core.internal.Numbers
+
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
-        validateNumbers(numbers)
+        winningNumbersValidations(numbers)
     }
 
     // TODO: Implement additional functions
+
+    fun winningNumbersValidations(numbers: List<Int>) {
+        validateNumbers(numbers)
+        validateUniqueNumbers(numbers)
+    }
+
     fun validateNumbers(numbers: List<Int>) {
-        for (number in numbers) {
-            if ((number < 1 || number > 45))
-                throw IllegalArgumentException("[ERROR] Lotto numbers must be between 1 and 45.")
+        if (numbers.any { it < 1 || it > 45 })
+            throw IllegalArgumentException("[ERROR] Lotto numbers must be between 1 and 45.")
+    }
+
+    fun validateUniqueNumbers(numbers: List<Int>) {
+        if (numbers.size != numbers.distinct().size) {
+            throw IllegalArgumentException("[ERROR] Lotto numbers must be unique.")
         }
     }
+
+
 }
